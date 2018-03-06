b60929a4710433335d6a7b5abaa53054aab479cf
Daiane Bushey <daianebushey@gmail.com>
2018-03-06 00:29:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,13 @@
 class CashRegister
   attr_accessor :total, :discount, :title, :price, :quantity, :items
 
-  @items = []
-
   def initialize(discount = 0)
     @total = 0
     @discount = discount
   end
 
   def add_item(title, price, quantity = 1)
+    @items = []
     @title = title
     @price = price
     @quantity = quantity
